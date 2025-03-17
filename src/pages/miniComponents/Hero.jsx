20c97c0900026ae1f32df5c648df124c795238fe
Mathew Alex <mathewalexmasai@gmail.com>
2025-03-17 21:20:27 +0530

    change the //

--- src/pages/miniComponents/Hero.jsx
+++ src/pages/miniComponents/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
   useEffect(() => {
     const getMyProfile = async () => {
       const { data } = await axios.get(
-        "https://backend-for-portfolio-git-main-mathewalexkeralas-projects.vercel.app//api/v1/user/portfolio/me",
+        "https://backend-for-portfolio-git-main-mathewalexkeralas-projects.vercel.app/api/v1/user/portfolio/me",
         { withCredentials: true }
       );
       console.log(data);
